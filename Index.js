d8d0b0acb467d5470c481558d6f462bbc3a6b4cb
Tony Jiang <tonyjiang@macbook-pro.lan>
2020-05-31 15:51:44 -0700

    Revert "expo ejecting"
    This reverts commit 46abd4a00a0d9d7d11ca77f0f077849daa27907b.

--- Index.js
+++ Index.js
@@ -1,8 +1,100 @@
-import { registerRootComponent } from 'expo';
+import 'react-native-gesture-handler';
+import React, { useState, useEffect, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { AsyncStorage } from 'react-native';
+import { loadUser } from './actions/auth';
+import Login from './components/Login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './components/Home';
+import Loading from './components/layout/Loading';
+import Landing from './components/Business/Landing';
+import Searching from './components/Business/Searching';
+import BusinessPage from './components/Business/BusinessPage';
+import { Provider } from 'react-redux';
+import store from './store';
+import Account from './components/Account';
+import Checkin from './components/Checkin';
+import SearchLoading from './components/layout/SearchLoading';
+import ExampleBusinessPage from './components/Business/ExampleBusinessPage';
+import UnverifiedBusinessPage from './components/Business/UnverifiedBusinessPage';
+import Signup from './components/Signup';
+import CheckedIn from './components/Checkedin';
+import BusinessHistoryList from './components/Business/BusinessHistoryList';
+import Favorites from './components/Business/Favorites';
+import UserReservations from './components/Business/UserReservations';
 
-import App from './App';
+const Stack = createStackNavigator();
+const Index = ({ auth, User }) => {
+    const [isAuth, setAuth] = useState(0);
+    useEffect(() => {
+        console.log("Auth State Changed");
+        console.log("Running Auth Check");
+        const isAuthenticated = async () => {
+            const token = await AsyncStorage.getItem('token');
+            if (token) {
+                store.dispatch(loadUser());
+                setAuth(2);
+                console.log("Is Authenticated");
+            } else {
+                console.log("Not Authenticated");
+                setAuth(1);
+            }
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in the Expo client or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+        };
+        if (auth.isAuthenticated) {
+            setAuth(2);
+            store.dispatch(loadUser());
+            console.log("Is Authenticated First Time");
+        } else {
+            console.log("Checking local storage");
+            isAuthenticated();
+        }
+
+    }, [auth]);
+    const authController = () => {
+        if (isAuth === 1) {
+            return (
+                <Fragment>
+                    <Stack.Screen name="Login" component={Login} />
+                    <Stack.Screen name="Signup" component={Signup} />
+                </Fragment>
+            );
+        }
+        if (isAuth === 2) {
+            return (
+                <Fragment>
+                    <Stack.Screen name="Landing" component={Landing} />
+                    <Stack.Screen name="Searching" component={Searching} />
+                    <Stack.Screen name="BusinessPage" component={BusinessPage} />
+                    <Stack.Screen name="Account" component={Account} />
+                    <Stack.Screen name="Checkin" component={Checkin} />
+                    <Stack.Screen name="SearchLoading" component={SearchLoading}></Stack.Screen>
+                    <Stack.Screen name="ExampleBusinessPage" component={ExampleBusinessPage} />
+                    <Stack.Screen name="UnverifiedBusinessPage" component={UnverifiedBusinessPage} />
+                    <Stack.Screen name="CheckedIn" component={CheckedIn}/>
+                    <Stack.Screen name="BusinessHistoryList" component={BusinessHistoryList}/>
+                    <Stack.Screen name="Favorites" component={Favorites}/>
+                    <Stack.Screen name="UserReservations" component={UserReservations}/>
+                </Fragment>
+            );
+        }
+    };
+    return (
+
+        <NavigationContainer>
+            {isAuth === 0 ? <Loading /> :
+                <Stack.Navigator screenOptions={{ headerShown: false }}>
+                    {authController()}
+                </Stack.Navigator>
+            }
+        </NavigationContainer>
+    );
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    User: state.user
+});
+
+export default connect(mapStateToProps, { loadUser })(Index);
